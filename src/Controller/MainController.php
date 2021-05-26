1ebbcbc634e12877b2ee176f5f2b84b75d8b7755
Shil Nailo <a@a.a>
2021-05-26 10:35:33 +0200

    Integration page d'acceuil

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
-
+use App\Entity\Article;
 
 
 class MainController extends AbstractController
@@ -14,12 +14,21 @@ class MainController extends AbstractController
     /**
      * Controleur de la page d'accueil du site
      * @Route("/", name="main_home")
-     * @Security("is_granted('ROLE_USER')")
      */
-    public function index(): Response
+    public function home(): Response
     {
+
+        $articleRepo= $this->getDoctrine()->getRepository(Article::class);
+
+        // Récupération du paramètre pour savoir combien d'articles seront affichés sur l'acceuil
+        $articlesNumber = $this->getParameter('homepage_articles_number');
+
+        // Récupération des derniers articles publiés
+        $articles = $articleRepo->findBy([], ['publicationDate' => 'DESC'], $articlesNumber);
+
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+            'articles' => $articles,
         ]);
     }
 
